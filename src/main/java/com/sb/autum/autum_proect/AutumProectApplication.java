b9f2ee29d1e0ed8c2dfbc9ced9642ca4be6b52b4
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-04-14 15:46:49 +0530

    Initial Commit

---
+++ src/main/java/com/sb/autum/autum_proect/AutumProectApplication.java
@@ -0,0 +1,13 @@
+package com.sb.autum.autum_proect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumProectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutumProectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/autum/autum_proect/AutumProectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.autum.autum_proect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutumProectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
