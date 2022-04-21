0446cf55062eaae7e22df21853bc2025f496c8c4
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-04-21 12:04:33 +0530

    Spring LifeCycle Methods: init and Destroy

---
+++ src/main/java/com/autum_project/lc/xmlconfig/LCconfig.java
@@ -0,0 +1,18 @@
+package com.autum_project.lc.xmlconfig;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LCconfig {
+	private static AbstractApplicationContext ctx;
+
+	public static void main(String[] args) {
+		ctx = new ClassPathXmlApplicationContext("com/autum_project/lc/xmlconfig/LCconfig.xml");
+		Patient patient = (Patient) ctx.getBean("patient");
+		System.out.println("Patient:" + patient);
+		// We changed the ApplicationContext to its parent class AbstractApplicationCOntext to aquire the additional property of Pre-Shutdown hook
+		// To enable the execution of destroy method of Patient bean
+		// registerShutdownHook() runs the destroy method
+		ctx.registerShutdownHook();
+	}
+}
